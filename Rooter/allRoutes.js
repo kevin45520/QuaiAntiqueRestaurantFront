d620a1e4577c340f9312cb4fc419e63c34d1cd53
kevin <raiton@live.fr>
2025-02-24 17:23:02 +0100

    ajout-systeme-routage

---
+++ Rooter/allRoutes.js
@@ -0,0 +1,13 @@
+import Route from "./Route.js";
+
+
+//Définir ici vos routes
+
+export const allRoutes = [
+
+    new Route("/", "Accueil", "/Pages/home.html"),];
+
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+
+export const websiteName = "Quai Antique";
